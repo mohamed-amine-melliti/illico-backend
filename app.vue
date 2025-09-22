1ba5a738bd328b4786c17cdf246f8b239b27a5b8
mohamedaminemelliti <mohamedamine.mellit@outlook.com>
2025-09-22 11:38:58 +0100

    22/09 4:56

--- app.vue
+++ app.vue
@@ -26,7 +26,7 @@ useHead({
   },
 })
 
-const title = 'Nuxt Shadcn UI - Dashboard Template'
+const title = 'Illico Back-office'
 const description = 'This dashboard, built with Nuxt, Shadcn UI, and UnoCSS. It includes a dark mode toggle and is optimized for performance and data efficiency.'
 
 useSeoMeta({
